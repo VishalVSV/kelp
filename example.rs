9a4af1298d2112fa0cdd9032bebbfbc9acb10687
VishalVSV <vishalsvetrivel@gmail.com>
2021-03-06 12:34:12 +0530

    Added gif

---
+++ example.rs
@@ -0,0 +1,16 @@
+fn main() -> Result<(), Box<dyn Error>> {
+    let mut i = 0;
+
+    let long_string = "
+                        multi line string highlighting!
+                      "
+
+    while i <= 10 {
+        i += 1;
+        println!("{}",i);
+        // Comments??
+        /*
+            Multi line syntax highlighting too
+        */
+    }
+}
